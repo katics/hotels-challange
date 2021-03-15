660ad0a70f041f7ecb8965357dcad9aaca8f93fb
Sasa Katic <sasa.katic@symphony.is>
2021-03-15 13:51:25 +0100

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.scss";
+
+const App: React.FunctionComponent = () => {
+  return (
+    <div>
+      app js ucitan
+      <p>learn react</p>
+    </div>
+  );
+};
+
+export default App;
